fdae37787b244440128c62b3eab0b86071075f2d
Gb-codertech <gbvieira2007@gmail.com>
2025-02-12 10:50:53 +0000

    Add files via upload

---
+++ Exercicio18/Exercicio18/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Exercicio18
+{
+    public static void Main(string[] args)
+    {
+        // Arrays para armazenar Nomes das cidades e a população
+        string[] nomesCidades = { "São Paulo", "Rio De Janeira", "Belo Horizonte", "Natal", "Lisboa", "Maringá", "Porto", "Governador Celso Ramos", "Coimbra"};
+        int[] populacao = { 11500000, 6220000, 2320000, 751000, 545000, 410000, 237000, 146000, 143000 };
+
+        // Percorrer arrays com FOR
+        Console.WriteLine("Cidades e a suas populações:");
+        for (int contador = 0; contador < nomesCidades.Length; contador++)
+        {
+            Console.WriteLine($"{nomesCidades[contador]} - {populacao[contador]} Habitantes");
+        }
+    }
+}
