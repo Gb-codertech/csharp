6ddfa1a87cdbbd67d393697454e1ecc72813396e
Gb-codertech <gbvieira2007@gmail.com>
2025-02-12 10:20:10 +0000

    Add files via upload

---
+++ Exercicio17/Exercicio17/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Exercicio17
+{
+    public static void Main(string[] args)
+    {
+        // Arrays para armazenar nomes dos filmes e anos de lançamento
+        string[] nomesFilmes = { "Harry Potter", "Zorro", "Simpsons" };
+        int[] anosLancamento = { 1999, 2010, 2014 };
+
+        // Percorrer arrays com FOR
+        Console.WriteLine("Filmes percorridos com for:");
+        for (int contador = 0; contador < nomesFilmes.Length; contador++)
+        {
+            Console.WriteLine("Filme: " + (nomesFilmes[contador]) +  " - Ano: " + (anosLancamento[contador]));
+        }
+    }
+}
